22c54ba4f13a8ee78e954b1668ecd0b259cb0786
AliakseiK1 <aliaksei.krasnahir@gmail.com>
2022-09-12 00:48:04 +0300

    Newest version of BMI calc with minor improvements

--- Aliaksei_KrasnahirHW2.py
+++ Aliaksei_KrasnahirHW2.py
@@ -1,40 +1,58 @@
 print("\nДобро пожаловать в BMI калькулятор")
 
-H = float(input("\nВведите свой рост, см: "))
-W = float(input("\nВведите свой вес, кг: "))
+hight = float(input("\nВведите свой рост, см: "))
+weight = float(input("\nВведите свой вес, кг: "))
+sex = str(input("\nВаш пол (m или w): "))
 
 #формула: I = W(кг)/H(м)*0.01(перевод в метры)**2
 
-I = int(W/(H * 0.01)**2)
+bmi_index = int(weight/(hight * 0.01)**2)
 
-# Стандартная шкала: 16 - 40
+#Стандартная шкала: 16 - 40
 
-#Diff = H-W
-Start = int(16)
-Finish = int(40)
+start = int(16)
+finish = int(40)
 
 print("\n")
 
 print("Ваш индекс на шкале BMI:")
-print("|", "-" * (I - Start+2), "|", "-" * (Finish-I+1), "|")
-print(Start, ">" * (I - Start), I, ">" * (Finish-I), Finish)
+print("|", "-" * (bmi_index - start+2), "|", "-" * (finish-bmi_index+1), "|")
+print(start, ">" * (bmi_index - start), bmi_index, ">" * (finish-bmi_index), finish)
 
 
-input("\nЧтобы увидеть расшифровку, нажмите Enter..")
+input("\nЧтобы посмотреть рекомендации, нажмите Enter..")
 
-if I < 16:
-    print('\nВыраженный дефицит массы тела')
-elif 16 <= I < 18.5:
-    print('\nНедостаточная масса тела (дефицит)')
-elif 18.5 <= I < 25:
-    print('\nНорма')
-elif 25 <= I < 30:
-    print('\nИзбыточная масса тела (состояние, предшествующее ожирению))')
-elif 30 <= I < 35:
-    print('\nОжирение 1-й степени)')
-elif 35 <= I < 40:
-    print('\nОжирение 2-й степени)')
+#if bmi_index < 16:
+#    print('\nВыраженный дефицит массы тела')
+#elif 16 <= bmi_index < 18.5:
+#    print('\nНедостаточная масса тела (дефицит)')
+#elif 18.5 <= bmi_index < 25:
+#    print('\nНорма')
+#elif 25 <= bmi_index < 30:
+#    print('\nИзбыточная масса тела (состояние, предшествующее ожирению))')
+#elif 30 <= bmi_index < 35:
+#    print('\nОжирение 1-й степени)')
+#elif 35 <= bmi_index < 40:
+#    print('\nОжирение 2-й степени)')
+#else:
+#    print('\nОжирение 3-й степени')
+
+if sex == "m":
+    if bmi_index < 16:
+            print('\nСрочно наберите массу!')
+    elif 16 <= bmi_index < 25:
+            print('\nЗапишитесь в тренажерный зал!')
+    elif 25 <= bmi_index < 40:
+            print('\nПробуйте сбалансировать питание!')
 else:
-    print('\nОжирение 3-й степени')
+    if bmi_index < 16:
+            print('\nПопробуйте сменить диету на более колорийную')
+    elif 16 <= bmi_index < 25:
+            print('\nЗаймитесь фитнесом!')
+    elif 25 <= bmi_index < 40:
+            print('\nНачните с пробежек по утрам!')
+
+
+
 
 input ("\nЧтобы выйти, нажмите Enter..")
