94aed10215d7398ff3568652fc8f64c69913918d
AliakseiK1 <aliaksei.krasnahir@gmail.com>
2022-09-15 18:26:40 +0300

    Third draft of HW4

--- Aliaksei_KrasnahirHW4.py
+++ Aliaksei_KrasnahirHW4.py
@@ -4,24 +4,23 @@
 #3. Декорируем эту функцию декоратором который проверяет если пользовател - админ (получили на первом этапе, то выводит сумму счета (выполняет функ из п 2)
 #4. Если не админ - Сумму не выводить (функцию даже не выполнять) а выводить - доступ запрещен 
 
-user_name = str(input("\nИмя пользователя: ")) # Ввести имя пользователя
+user_name = input("\nИмя пользователя: ")
 
-account_ballance = "23.34$"
+def account_ballance(*args, **kwargs):
+    if user_name == "admin":
+     print("23.34$") #создаём функцию, которая выводит захардкоданный баланс счёта
 
-#def account_ballance():
-        #print("23.34$") #создаём функцию, которая выводит захардкоданный баланс счёта
+#user = admin - only admin for the application
 
 
-#user = Valera - only admin for the application
-
-def decologin(*args, **kwargs):
+def decologin(account_ballance):
    def wrapper_decorator(*args, **kwargs):
-    if user_name != "Valera":
+    if user_name != "admin":
         return "В доступе отказано!"
+    account_ballance(*args, **kwargs)
     return wrapper_decorator
 
 @decologin
-def account_ballance1():
-    print(account_ballance)
-
-account_ballance1()
+def account_ballance():
+    return(account_ballance)
+account_ballance()
