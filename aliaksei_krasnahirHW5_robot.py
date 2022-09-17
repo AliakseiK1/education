d1324dcba6c698ded5e2dd49384f7e8a45451cfe
AliakseiK1 <aliaksei.krasnahir@gmail.com>
2022-09-17 23:58:49 +0300

    First implementation

--- aliaksei_krasnahirHW5_robot.py
+++ aliaksei_krasnahirHW5_robot.py
@@ -1,2 +1,33 @@
 #Homework related to calculation of robot movement
 
+"""
+import sys
+
+print("Enter the array:\n")   
+userInput = sys.stdin.readlines()
+print(userInput)
+
+"""
+
+"""
+a = input().split()
+
+print(a)
+
+for a 
+
+"""
+
+import math
+
+up_steps_to_enter = int(input("\nУкажите, на сколько шагов вверх должен подняться робот (целое положительное число): "))
+down_steps_to_enter = int(input("\nУкажите, на сколько шагов вниз должен опуститься робот (целое положительное число): "))
+left_steps_to_enter = int(input("\nУкажите, на сколько шагов влево должен сместиться робот (целое положительное число): "))
+right_steps_to_enter = int(input("\nУкажите, на сколько шагов вправо должен подняться робот (целое положительное число): "))
+
+a = abs(up_steps_to_enter - down_steps_to_enter)
+print(a)
+b = abs(left_steps_to_enter - right_steps_to_enter)
+print(b)
+distanse = math.sqrt(a ** 2 + b ** 2)
+print(distanse)
