1a1cbefe547eb9bde409ae79fa97671e5036aa69
AliakseiK1 <aliaksei.krasnahir@gmail.com>
2022-09-19 13:46:51 +0300

    latest implemenation of HW5 - robot

--- aliaksei_krasnahirHW5_robot.py
+++ aliaksei_krasnahirHW5_robot.py
@@ -1,22 +1,8 @@
 #Homework related to calculation of robot movement
 
-"""
-import sys
 
-print("Enter the array:\n")   
-userInput = sys.stdin.readlines()
-print(userInput)
+#Version 1 - Simplified
 
-"""
-
-"""
-a = input().split()
-
-print(a)
-
-for a 
-
-"""
 import math
 
 print("\nДобро пожаловать в систему управления роботом!\n")
@@ -34,3 +20,43 @@ side_b = left_steps_to_enter - right_steps_to_enter
 hypotenuse_с = int(math.sqrt(side_a ** 2 + side_b ** 2))
 #hypotenuse_с = str(hypotenuse_с)
 print("\nРобот отошел на расстояние, равное {0} шаг(ов)" .format(hypotenuse_с))
+
+"""
+#Version 2 - advanced
+
+import array
+
+
+spisok = []
+input_value = input("\nУкажите, на сколько шагов сместиться роботу согласно правилам ниже:"
++ "\n - 1. Сместиться вверх = \'U*\', где \'*\' - любое целое число"
++ "\n - 2. Сместиться вниз = \'D*\', где \'*\' - любое целое число"
++ "\n - 3. Сместиться влево = \'L*\', где \'*\' - любое целое число"
++ "\n - 4. Сместиться вправо = \'R*\', где \'*\' - любое целое число"
++ "\nЧтобы закончить ввод, нажмите Enter...\n: ")
+
+while input_value != "":
+    input_value = input("\nУкажите, на сколько шагов сместиться роботу согласно правилам ниже:"
++ "\n - 1. Сместиться вверх = \'U*\', где \'*\' - любое целое число"
++ "\n - 2. Сместиться вниз = \'D*\', где \'*\' - любое целое число"
++ "\n - 3. Сместиться влево = \'L*\', где \'*\' - любое целое число"
++ "\n - 4. Сместиться вправо = \'R*\', где \'*\' - любое целое число"
++ "\nЧтобы закончить ввод, нажмите Enter...\n: ")
+    spisok.append(input_value)
+    if input_value == "":
+        print("Ввод закончен")
+        continue
+    
+print(spisok)
+spisok1 = spisok[0:-1]
+print(spisok1["U"])
+
+
+
+print("Количество \"нд\"=", spisok.startwith ("U"))
+
+print("Количество \"нд\"=", spisok.count ("U"))
+print("Количество \"ам\"=", spisok.count("D"))
+print("Количество \"о\"=", spisok.count("L"))
+print("Количество \"о\"=", spisok.count("R"))
+"""
---
+++ aliaksei_krasnahirHW5_robot_New.py
@@ -0,0 +1,42 @@
+import math
+
+x = 0 #start point for robot on axis X
+y = 0 #start point for robot on axis Y
+
+input_value = input("\nУкажите, на сколько шагов сместиться роботу согласно правилам ниже:" 
++ "\n - 1. Сместиться вверх = \'U*\', где \'*\' - любое целое число"
++ "\n - 2. Сместиться вниз = \'D*\', где \'*\' - любое целое число"
++ "\n - 3. Сместиться влево = \'L*\', где \'*\' - любое целое число"
++ "\n - 4. Сместиться вправо = \'R*\', где \'*\' - любое целое число"
++ "\nЧтобы закончить ввод, введите \'e\', нажмите Enter...\n: ").split() #definition of input: first function
+if input_value[0] in ("u","d"):
+    y = y - int(input_value[1])
+    print(y)
+else:
+    if input_value[0] in ("r","l"):
+     x = x - int(input_value[1])
+     print(x)
+
+while True:
+    input_value = input("\nУкажите, на сколько шагов сместиться роботу согласно правилам ниже:"
++ "\n - 1. Сместиться вверх = \'U*\', где \'*\' - любое целое число"
++ "\n - 2. Сместиться вниз = \'D*\', где \'*\' - любое целое число"
++ "\n - 3. Сместиться влево = \'L*\', где \'*\' - любое целое число"
++ "\n - 4. Сместиться вправо = \'R*\', где \'*\' - любое целое число"
++ "\nЧтобы закончить ввод, введите \'e\', нажмите Enter...\n: ").split() #definition of input in function
+    if input_value[0] in ("u","d"):
+       y = y - int(input_value[1])
+       print(y)
+    elif input_value[0] in ("r","l"):
+           x = x - int(input_value[1])
+           print(x)
+    else:
+        if input_value[0] in ("e"):
+         break
+side_a = x
+
+side_b = y
+
+hypotenuse_с = int(math.sqrt(side_a ** 2 + side_b ** 2))
+#hypotenuse_с = str(hypotenuse_с)
+print("\nРобот отошел на расстояние, равное {0} шаг(ов)" .format(hypotenuse_с))
