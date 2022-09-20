1dbeec477f89e0cdda389a44487fd1d2dfc41399
AliakseiK1 <aliaksei.krasnahir@gmail.com>
2022-09-21 00:52:43 +0300

    Second draft of Clock

--- aliaksei_krasnahirHW6_clock.py
+++ aliaksei_krasnahirHW6_clock.py
@@ -1,10 +1,24 @@
 import datetime
 import time
 import os
+import builtins
 
 
+
+print(u"\u2B1C"+u"\u2B1C"+u"\u2B1C"+u"\u2B1C"+u"\u2B1C"+u"\u2B1C\n"
+    + u"\u2B1C"+u"\u2B1C"+u"\u2B1C"+u"\u2B1C"+u"\u2B1C"+u"\u2B1C\n"
+    + u"\u2B1C"+u"\u2B1C"+u"\u2B1B"+u"\u2B1B"+u"\u2B1C"+u"\u2B1C\n"
+    + u"\u2B1C"+u"\u2B1C"+u"\u2B1B"+u"\u2B1B"+u"\u2B1C"+u"\u2B1C\n"
+    + u"\u2B1C"+u"\u2B1C"+u"\u2B1B"+u"\u2B1B"+u"\u2B1C"+u"\u2B1C\n"
+    + u"\u2B1C"+u"\u2B1C"+u"\u2B1B"+u"\u2B1B"+u"\u2B1C"+u"\u2B1C\n"
+    + u"\u2B1C"+u"\u2B1C"+u"\u2B1C"+u"\u2B1C"+u"\u2B1C"+u"\u2B1C\n"
+    + u"\u2B1C"+u"\u2B1C"+u"\u2B1C"+u"\u2B1C"+u"\u2B1C"+u"\u2B1C\n")
+"""
 while True:
     current_sys_time = datetime.datetime.now().time()
     print("%s:%s:%s" % (current_sys_time.hour, current_sys_time.minute, current_sys_time.second))
     time.sleep(0.2)
     os.system('clear')
+
+"""
+
